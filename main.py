997dd4b506e03b951485daffdb26a0a5856f243a
mayankdubey0 <mayankd3@illinois.edu>
2025-10-04 16:45:41 -0500

    Working MC Control

--- main.py
+++ main.py
@@ -23,91 +23,81 @@ nS = len(S)
 
 state, info = env.reset()
 
-def MC_policy_eval(pi, b_0=0, gamma=0.9, max_iter=10000):
-    V = np.random(nS)
-
-    states = []
-    actions = []
-    rewards = []
-    state_count = np.zeros(nS)
-    s = b_0
-
-    for t in range(max_iter): # runs each episode
-        a = pi[s] # choose action
-        actions.append(a)
-        s_prime, r, done, _, _ = env.step(a) # take action and move to next state
-        states.append(s_prime)
-        rewards.append(r) # add reward
-        if done:
-            break
-        s = s_prime
-        if s in states:
-            state_count[s] += 1
-    
-    G = 0
-    visited_states = set()
-    for t in range(len(states)):
-        G = rewards[t] + gamma*G
-        if states[t] not in visited_states: # first visit
-            V[states[t]] += (1/state_count[states[t]]) * (G - V(states[t]))
-            states.add(states[t])
+def plot_graph(time_steps, returns):
+    plt.plot(time_steps, returns)
+    plt.show()
 
 
-def MC_control(b_0, gamma=0.95, epsilon_s=0.1, max_iter=10000):
+def MC_control(b_0=None, gamma=0.95, epsilon_s=0.3, max_iter=200, episodes=10000, eps_decay=0.99):
     Q = np.random.rand(nS, nA)
     s_a_count = np.zeros((nS, nA))
+    returns = []
+    times = []
+    mean_returns = []
+    time_steps = 0
+    epsilon = epsilon_s
 
-    for i in range(100):
-        # print("episode:",i)
-        states = []
-        actions = []
-        rewards = []
-        epsilon = epsilon_s
-
+    for ep in range(episodes):
         s, _ = env.reset()
-        env.unwrapped.s = b_0
-        s = b_0   
+        print(time_steps)
+        if b_0 is not None:
+            env.unwrapped.s = int(b_0)
+            s = int(b_0)
+
+        states, actions, rewards = [], [], []
 
         for t in range(max_iter):
+            time_steps +=1
             if np.random.rand() < epsilon:
                 a = np.random.randint(nA)
-            else:                              
+            else:
                 a = np.argmax(Q[s])
-            epsilon = epsilon * 0.9
 
             states.append(s)
             actions.append(a)
 
-            s_prime, r, done, _, _ = env.step(a)
+            s_next, r, terminated, truncated, _ = env.step(a)
+            done = terminated or truncated
             rewards.append(r)
+            s = s_next
+            # time_steps += 1
 
             if done:
+                # print("Reward:", r)
                 break
-            s = s_prime
 
         G = 0.0
-        visited_s_a = set()
+        visited = set()
         for t in reversed(range(len(states))):
             G = rewards[t] + gamma * G
             sa = (states[t], actions[t])
-            if sa not in visited_s_a:
-                visited_s_a.add(sa)
-                s_a_count[sa] += 1
-                Q[sa] += (G - Q[sa]) / s_a_count[sa]
+            if sa not in visited:
+                visited.add(sa)
+                s_a_count[sa] += 1.0
+                alpha = 0.2 #1.0 / s_a_count[sa]
+                Q[sa] += alpha * (G - Q[sa])
+        time_steps += len(states)
+        times.append(time_steps)
+        returns.append(G)
+        mean_returns.append(np.mean(returns))
 
         pi = np.argmax(Q, axis=1)
-    return Q, pi
+
+        epsilon = epsilon * eps_decay
+
+    pi = np.argmax(Q, axis=1)
+    return Q, pi, mean_returns, times
 
 
 def SARSA(b_0, gamma=0.95, alpha=0.2, epsilon_s=0.1, max_iter=10000):
     Q = np.random.rand(nS, nA)
+    epsilon = epsilon_s
 
-    for i in range(10000):
+    for i in range(500):
         # print("episode:",i)
         states = []
         actions = []
         rewards = []
-        epsilon = epsilon_s
 
         s, _ = env.reset()
         env.unwrapped.s = b_0
@@ -152,13 +142,13 @@ def SARSA(b_0, gamma=0.95, alpha=0.2, epsilon_s=0.1, max_iter=10000):
 
 def Q_learning(b_0, gamma=0.95, alpha=0.2, epsilon_s=0.1, max_iter=10000):
     Q = np.random.rand(nS, nA)
+    epsilon = epsilon_s
 
-    for i in range(100):
+    for i in range(500):
         # print("episode:",i)
         states = []
         actions = []
         rewards = []
-        epsilon = epsilon_s
 
         s, _ = env.reset()
         env.unwrapped.s = b_0
@@ -186,13 +176,11 @@ def Q_learning(b_0, gamma=0.95, alpha=0.2, epsilon_s=0.1, max_iter=10000):
                 a_prime = np.argmax(Q[s_prime])
             epsilon = epsilon * 0.9
 
-            sa_p = (s_prime, a_prime)
-
             if done:
                 Q[sa] += alpha*(rewards[t] - Q[sa])
                 break
             
-            Q[sa] += alpha*(rewards[t]+gamma*np.argmax(Q[sa_p]) - Q[sa])
+            Q[sa] += alpha*(rewards[t]+gamma*np.argmax(Q[s_prime]) - Q[sa])
 
             s = s_prime
             a = a_prime
@@ -200,11 +188,19 @@ def Q_learning(b_0, gamma=0.95, alpha=0.2, epsilon_s=0.1, max_iter=10000):
         pi = np.argmax(Q, axis=1)
     return Q, pi
 
-# Q, pi = MC_control(0)
-# Q, pi = SARSA(0)
-Q, pi = Q_learning(0)
+print("Running MC...")
+Q_MC, pi_MC, r, t  = MC_control(0)
+print(Q_MC)
+plot_graph(t, r)
+print("MC Policy:", pi_MC,"\n")
+# print("Running SARSA...")
+# Q_SARSA, pi_SARSA = SARSA(0)
+# print("SARSA Policy:", pi_SARSA,"\n")
+# print("Running Q...")
+# Q_Q, pi_Q = Q_learning(0)
+# print("Q Policy:", pi_Q,"\n")
 
 # print("Optimal Action Value Function:\n", Q, "\n\n")
-print("Optimal Policy:\n", pi)
+
 
 
